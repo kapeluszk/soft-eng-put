b1bbebcd8346e86f43577402ac742b379336793d
kacper <126521076+kapeluszk@users.noreply.github.com>
2023-12-01 18:41:45 +0100

    dodałem endpointy (na razie nic w nich nie ma) + jest endpoint ping

--- main.go
+++ main.go
@@ -1,72 +1,73 @@
 package main
 
 import (
-	"embed"
 	"fmt"
-	"html/template"
-	"log"
-	"net/http"
+	"github.com/gin-gonic/gin"
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
-
-	"github.com/jritsema/gotoolbox"
-	"github.com/jritsema/gotoolbox/web"
 )
 
-var (
-	//go:embed all:templates/*
-	templateFS embed.FS
-
-	//go:embed css/output.css
-	css embed.FS
-
-	//parsed templates
-	html *template.Template
-)
+//var (
+//	//go:embed all:templates/*
+//	templateFS embed.FS
+//
+//	//go:embed css/output.css
+//	css embed.FS
+//
+//	//parsed templates
+//	html *template.Template
+//)
 
 func main() {
 
 	//exit process immediately upon sigterm
 	handleSigTerms()
 
-	//parse templates
-	var err error
-	html, err = web.TemplateParseFSRecursive(templateFS, ".html", true, nil)
-	if err != nil {
-		panic(err)
-	}
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
 
-	//add routes
-	router := http.NewServeMux()
-	router.Handle("/css/output.css", http.FileServer(http.FS(css)))
+	r.GET("/tickets", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "tickets",
+		})
+	})
 
-	router.Handle("/company/add", web.Action(companyAdd))
-	router.Handle("/company/add/", web.Action(companyAdd))
+	r.GET("/tickets/:id", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "id",
+		})
+	})
 
-	router.Handle("/company/edit", web.Action(companyEdit))
-	router.Handle("/company/edit/", web.Action(companyEdit))
+	r.POST("/login", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
 
-	router.Handle("/company", web.Action(companies))
-	router.Handle("/company/", web.Action(companies))
+	r.POST("/tickets", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
 
-	router.Handle("/", web.Action(index))
-	router.Handle("/index.html", web.Action(index))
+	r.POST("/tickets/:id/close", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
 
-	//logging/tracing
-	nextRequestID := func() string {
-		return fmt.Sprintf("%d", time.Now().UnixNano())
-	}
-	logger := log.New(os.Stdout, "http: ", log.LstdFlags)
-	middleware := tracing(nextRequestID)(logging(logger)(router))
+	r.POST("/tickets/:id/assing", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
 
-	port := gotoolbox.GetEnvWithDefault("PORT", "8080")
-	logger.Println("listening on http://localhost:" + port)
-	if err := http.ListenAndServe(":"+port, middleware); err != nil {
-		logger.Println("http.ListenAndServe():", err)
-		os.Exit(1)
-	}
+	r.Run()
 }
 
 func handleSigTerms() {
